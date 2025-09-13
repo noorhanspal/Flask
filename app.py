25e1b47e7c15e54fb1058e5750a00af18fe8be2f
noorhanspal <deepnoor977@gmail.com>
2025-09-13 23:30:52 +0530

    Added Todo.db, index.html template, and updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.title}"
 
 @app.route('/')
 def hello_world():
-    return 'Hello,world'
+    todo = Todo (title= "First Todo", desc = "start investing in stock market")
+    db.session.add(todo)
+    db.session.commit()
+    return render_template('index.html')
+    # Agar sirf text chahiye to:
+    # return 'Hello, world'
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
